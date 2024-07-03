272b5c0a8fba94f4f626ca987787310ebdb094db
Kate <Kate@DESKTOP-VDHFHMO>
2024-07-03 15:00:34 +0300

    Описать сущность Kaki

---
+++ domain/Store/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace Store;
+
+public class Kaki
+{
+    public int Id { get; } 
+    public string Title { get; }
+    public Kaki(int id, string title)
+    { 
+        Id = id;
+        Title = title; 
+    }
+}
